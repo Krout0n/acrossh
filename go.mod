7159e42b4c31dbd13be5010d2e006d143d352dae
Krouton <krouton@hatena.ne.jp>
2020-11-10 18:31:06 +0900

    Parse simple acrossh.conf

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.14
 
 require (
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
-	gopkg.in/ini.v1 v1.62.0 // indirect
+	gopkg.in/ini.v1 v1.62.0
 )
--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"os"
+
+	"gopkg.in/ini.v1"
+)
+
+type ConnectionConfig struct {
+	user    string
+	host    string
+	port    int
+	keyPath string
+}
+
+func ce(err error, msg string) {
+	if err != nil {
+		println(msg)
+		println(err.Error())
+		os.Exit(1)
+	}
+}
+
 func main() {
-	println("Hello, world!")
+	cfg, err := ini.Load("acrossh.conf")
+	ce(err, "config")
+	conf := ConnectionConfig{
+		// Users are "Krouton" if users don't provide thier username to config.
+		user:    cfg.Section("connection").Key("user").MustString("Krouton"),
+		host:    cfg.Section("connection").Key("host").MustString("localhost"),
+		port:    cfg.Section("connection").Key("port").MustInt(22),
+		keyPath: cfg.Section("connection").Key("key_path").MustString("~/.ssh/id_rsa"),
+	}
+	// example.com is expected.
+	println(conf.host)
 }
