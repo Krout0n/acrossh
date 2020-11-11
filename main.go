8f07cbc8f992f185a8f952e2b9dd0fa2e2ae2644
Krouton <krouton@hatena.ne.jp>
2020-11-11 10:13:26 +0900

    The pty was unnecessary.

--- main.go
+++ main.go
@@ -59,22 +59,4 @@ func main() {
 	ce(err, "new session")
 	defer session.Close()
 
-	session.Stdout = os.Stdout
-	session.Stderr = os.Stderr
-	session.Stdin = os.Stdin
-
-	modes := ssh.TerminalModes{
-		ssh.ECHO:          0,
-		ssh.TTY_OP_ISPEED: 14400,
-		ssh.TTY_OP_OSPEED: 14400,
-	}
-	term := os.Getenv("TERM")
-	err = session.RequestPty(term, 25, 80, modes)
-	ce(err, "request pty")
-
-	err = session.Shell()
-	ce(err, "start shell")
-
-	err = session.Wait()
-	ce(err, "return")
 }
